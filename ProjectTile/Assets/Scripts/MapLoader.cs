0161e13726fa105b2e5cdb74834c76ceca2bdbb5
Fan Ling <troyprag816@gmail.com>
2019-10-31 01:40:10 -0400

    Pathfinding

---
+++ ProjectTile/Assets/Scripts/MapLoader.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class MapLoader
+{   
+    public static Map ReadMap(int map_index)
+    {
+        int col;
+        int row;
+        int counter = 0;
+        string temp;
+        string path = "Assets/Maps/Map_"+map_index+".txt";
+        StreamReader reader = new StreamReader(path);
+
+        if(reader.Peek() == '/')
+        {
+            reader.ReadLine();
+        }
+
+        temp = reader.ReadLine();
+        row = int.Parse(temp.Split()[0]);
+        col = int.Parse(temp.Split()[1]);
+        Map map = new Map(col, row);
+
+        while ((temp = reader.ReadLine()) != null)
+        {
+            if (reader.Peek() != '/')
+            {
+                string[] temp_string = temp.Split();
+                for (int i = 0; i < map.row; i++)
+                {
+                    //Debug.Log(counter * row + i);
+                    map.mapData[counter * row + i] = int.Parse(temp_string[i]);
+                }
+            }
+            counter++;
+        }
+
+        reader.Close();
+        return map;
+    }
+}
+
+public struct Map
+{
+	public int col;
+	public int row;
+	public int[] mapData;
+
+    public Map(int _col, int _row)
+    {
+        col = _col;
+        row = _row;
+        mapData = new int[_col * _row];
+    }
+}
---
+++ ProjectTile/Assets/Scripts/MapManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MapManager : MonoBehaviour
+{
+    public GameObject tile;
+    public int map_index;
+    Map currentMap;
+
+    private void Awake()
+    {
+        foreach (Transform child in this.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void Start()
+    {       
+        currentMap = MapLoader.ReadMap(map_index);
+
+        for (int i = 0; i < currentMap.col; i++)
+        {
+            for (int j = 0; j < currentMap.row; j++)
+            {
+                if (currentMap.mapData[i * currentMap.col + j] == 1)
+                {
+                    Instantiate(tile, new Vector3(i, 0, j), Quaternion.identity, this.transform);
+                }
+            }
+        }
+    }
+}
